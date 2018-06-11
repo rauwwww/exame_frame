567ba81a6c599f2233e2253a62f57c6c4c1cd7ec
Someone <Patrickholsths@gmail.com>
2018-06-11 13:03:57 +0200

    Added first stock component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { StockComponent } from './stock/stock.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StockComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/stock/stock.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-stock',
+    templateUrl: './stock.component.html'
+})
+export class StockComponent {
+    
+}
