9cbebfe81762a04cb47ac19766ffce9dc2644158
Someone <Patrickholsths@gmail.com>
2018-06-11 14:22:15 +0200

    added stocks for stock components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { StockComponent } from './stock/stock.component';
+import { StocksComponent } from './stocks/stocks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StockComponent
+    StockComponent,
+    StocksComponent,
   ],
   imports: [
     BrowserModule,
