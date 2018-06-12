c8aac75e3e27e1e3b388b4c65b8e0f9005dcbbad
Someone <Patrickholsths@gmail.com>
2018-06-12 14:23:32 +0200

    client AUth login + signup setup

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,17 +6,23 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { StockComponent } from './stock/stock.component';
 import { StocksComponent } from './stocks/stocks.component';
+import { LoginComponent } from './login/login.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StockComponent,
     StocksComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/signup/signup.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from "@angular/router";
+import { Observable } from 'rxjs/Observable';
+import { tap, catchError } from 'rxjs/operators';
+import { of } from 'rxjs/observable/of';
+
+@Component({
+    selector: 'app-signup',
+    templateUrl: './signup.component.html',
+    styleUrls: ['./signup.component.css'],
+
+  })
+
+export class SignupComponent {
+
+    constructor(private http: HttpClient) { }
+
+    signupData = { username:'', password:'' };
+    message = '';
+
+    signup() {
+        this.http.post('/users/signup',this.signupData).subscribe(resp => {
+          console.log(resp);
+        
+        }, err => {
+          this.message = err.error.msg;
+        });
+      }
+
+}
