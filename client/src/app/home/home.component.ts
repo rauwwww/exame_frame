a5622c10eb72221bd6daf49cec5da9c9cab03e7e
Someone <Patrickholsths@gmail.com>
2018-06-13 16:59:57 +0200

    kom ikke med

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -51,12 +51,6 @@ login() {
 
   ngOnInit() {
     // checks if user is loggedin and has token
-    if (localStorage.getItem('jwtToken') != null) {
       this.getStocks();
-      console.log('localtoken:', localStorage.getItem('jwtToken'));
-    } else {
-      this.router.navigate(['login']);
-    }
   }
-
 }
